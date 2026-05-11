b5133f829094c979cf927c5700ecb307586457eb
semav-techdev <semavmh12@gmail.com>
2026-05-11 12:21:52 +0300

    change language

---
+++ README.md
@@ -0,0 +1,6 @@
+### run the project
+## first :
+npm install
+## second :
+npm run dev 
+then go to the link
---
+++ src/utils/helperArlet.js
@@ -0,0 +1,32 @@
+import {playBeep,pushAlert} from "../utils/alretUtils"
+// بيعطي التنبيه بعد مدة زمنية مدتها 2 ثانية
+export function handlePostureIssue({
+  condition,
+  startRef,
+  notifiedRef,
+  now,
+  message,
+  soundType,
+  setAlerts
+}) {
+  if (condition) {
+    if (startRef.current === null) startRef.current = now;
+
+    const elapsed = (now - startRef.current) / 1000;
+// اذا استمر التنبيه 2 ثانية يعطي صوت
+    if (elapsed >= 2 && !notifiedRef.current) {
+      playBeep(soundType);
+      pushAlert(setAlerts, message);
+
+      notifiedRef.current = true;
+      startRef.current = null;
+      notifiedRef.current = false;
+    }
+
+    return true; 
+  } else {
+    startRef.current = null;
+    notifiedRef.current = false;
+    return false;
+  }
+}
